de13722b1adc2c10d3eaadb5b6faff12380ffe55
mohamadsoufi <mohamad.soufi@outlook.com>
2020-11-05 05:42:51 +0100

    added contact us btn with toggle feature

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,3 +1,22 @@
-$(".navbar-toggler").on("click", function () {
-    $("#navbarTogglerDemo01").toggle();
-});
+(function () {
+    //toggling the hamburger btn.
+    $(".navbar-toggler").on("click", function () {
+        $("#navbarTogglerDemo01").toggle();
+    });
+
+    //toggling the contact us form.
+    $(".contact-us-btn").on("click", function (e) {
+        $(".contact-us-form-overlay").removeClass("hide");
+
+        $("#collapseContactForm").toggle();
+    });
+
+    $(".contact-us-form-container").on("click", function (e) {
+        e.stopPropagation();
+    });
+
+    //toggling the contact us form by clicking outside it.
+    $(".contact-us-form-overlay").on("click", function (e) {
+        $("#collapseContactForm").toggle();
+    });
+})();
